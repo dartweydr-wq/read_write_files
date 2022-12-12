f65762b6b6b1a67f7c419d00b704f5187c2acc14
Aleksey <badproper98@gmail.com>
2022-12-12 12:40:40 +0300

    чтение/запись данных

---
+++ go.mod
@@ -0,0 +1,3 @@
+module 06_task
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func logTime() {
+	start := time.Now()
+	duration := time.Since(start)
+	res := duration.Nanoseconds()
+
+	fmt.Printf("Время выполнения программы: %d наносекунд", res)
+}
+
+func file(filePath string) {
+	f1, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Unable to open file [%s]", err.Error())
+		os.Exit(1)
+	}
+	defer f1.Close()
+
+	createFile, err := os.Create("out.txt")
+	if err != nil {
+		fmt.Printf("Unable to create file [%s]", err.Error())
+		os.Exit(1)
+	}
+	defer createFile.Close()
+
+	text := ""
+	s := bufio.NewScanner(f1)
+	s.Split(bufio.ScanLines)
+	for s.Scan() {
+		text = s.Text()
+	}
+
+	writeFile, err := createFile.WriteString(text)
+	if err != nil {
+		fmt.Printf("Unable to write file [%s]", err.Error())
+	}
+	fmt.Println("Сколько байт записано: ", writeFile)
+}
+
+func main() {
+	path := "in.txt"
+	file(path)
+	defer logTime()
+}
